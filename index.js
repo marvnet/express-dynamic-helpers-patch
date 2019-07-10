9d39cb99e3c7a05747081b9cde3b39347db2c8c3
MagicMarvMan <marvin.kai@gmx.de>
2019-07-10 16:42:59 +0200

    Beautify code

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 // express response patch for dynamicHelpers support
 const response = require('express').response,
-  render = response.render;
+    render = response.render;
 
 
 module.exports = (app) => {
-  // add dynamicHelpers function to app object
-  app.dynamicHelpers = (helpers) => {
-    app._dynamicHelpers = app._dynamicHelpers || {};
-    for (let key in helpers) {
-      if (helpers.hasOwnProperty(key)) {
-        app._dynamicHelpers[key] = helpers[key];
-      }
-    }
-  };
+    // add dynamicHelpers function to app object
+    app.dynamicHelpers = (helpers) => {
+        app._dynamicHelpers = app._dynamicHelpers || {};
+        for (let key in helpers) {
+            if (helpers.hasOwnProperty(key)) {
+                app._dynamicHelpers[key] = helpers[key];
+            }
+        }
+    };
 };
 
 // patch render function
 response.render = function() {
-  var dynamicHelpers = this.app._dynamicHelpers;
+    var dynamicHelpers = this.app._dynamicHelpers;
 
-  if (dynamicHelpers) {
-    for (let key in dynamicHelpers) {
-      // call every helper and sign result to locals
-      this.locals[key] = dynamicHelpers[key].call(this.app, this.req, this);
+    if (dynamicHelpers) {
+        for (let key in dynamicHelpers) {
+            // call every helper and sign result to locals
+            this.locals[key] = dynamicHelpers[key].call(this.app, this.req, this);
+        }
     }
-  }
 
-  render.apply(this, arguments);
+    render.apply(this, arguments);
 };
